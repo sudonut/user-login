8180b53c489b7ed61c8e28110010bb713e7349ac
sudonut <paradoxotl@gmail.com>
2021-05-25 22:03:58 -0400

    Added some form validation with password fields

--- public/script/script.js
+++ public/script/script.js
@@ -1,9 +1,17 @@
-document.getElementById("user-form").addEventListener("submit", formValidate);
+// document.getElementById("user-form").addEventListener("submit", formValidate);
 
-function formValidate(e) {
-  e.preventDefault();
+function formValidate() {
   const userName = document.getElementById("user-name");
   const email = document.getElementById("email");
   const password = document.getElementById("password");
   const passwordConfirm = document.getElementById("password-confirm");
+
+  if (password.value != passwordConfirm.value) {
+    alert("Passwords do not match!");
+    password.style.borderColor = "#E34234";
+    passwordConfirm.style.borderColor = "#E34234";
+    return false;
+  } else {
+    return true;
+  }
 }
