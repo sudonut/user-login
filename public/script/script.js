2a3df152e2304ce7d2e83de4fed71ca35356ac13
sudonut <paradoxotl@gmail.com>
2021-06-01 16:53:16 -0400

    Removed json package

--- public/script/script.js
+++ public/script/script.js
@@ -1,5 +1,3 @@
-// document.getElementById("user-form").addEventListener("submit", formValidate);
-
 function formValidate() {
   const userName = document.getElementById("user-name");
   const email = document.getElementById("email");
@@ -7,7 +5,7 @@ function formValidate() {
   const passwordConfirm = document.getElementById("password-confirm");
 
   if (password.value != passwordConfirm.value) {
-    alert("Passwords do not match!");
+    alert("Passwords must match!");
     password.style.borderColor = "#E34234";
     passwordConfirm.style.borderColor = "#E34234";
     return false;
--- server/server.js
+++ server/server.js
@@ -5,26 +5,28 @@ const path = require("path");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-
 let mongoName = "",
-mongoPass = ""
+  mongoPass = "";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(`mongodb+srv://${mongoName}:${mongoPass}@auth-db.utpv2.mongodb.net/user-storage?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology:true})
+mongoose.connect(
+  `mongodb+srv://${mongoName}:${mongoPass}@auth-db.utpv2.mongodb.net/user-storage?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 mongoose.connection.on("error", (error) => {
-  console.log(error)
-})
+  console.log(error);
+});
 mongoose.connection.on("open", () => {
-  console.log("Connected to MongoDB Database successfully!")
-})
+  console.log("Connected to MongoDB Database successfully!");
+});
 
 const dataSchema = {
   username: String,
   password: String,
-  email: String
-}
+  email: String,
+};
 
 const User = mongoose.model("User", dataSchema);
 
@@ -34,14 +36,30 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  let newUser = new User({
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email
+  User.findOne({
+    $or: [{ email: req.body.email },
+    { username: req.body.username }]})
+    .then((user) => {
+    if (user) {
+      console.log(user);
+      let errors = {};
+      if (user.username === req.body.username) {
+        errors.username = "Username already exists";
+      } else {
+        errors.email = "Email already exists";
+      }
+      return console.log((errors));
+    } else {
+      let newUser = new User({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+      });
+      newUser.save();
+      res.redirect("/");
+      console.log(req.body);
+    }
   });
-  newUser.save();
-  res.redirect("/");
-  console.log(req.body);
 });
 
 app.listen(port, () => {
